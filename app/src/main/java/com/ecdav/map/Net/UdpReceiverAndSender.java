47a057656bcc077ce5651811cf0405f3fab684ce
Silicon <iamwanglinqing@163.com>
2017-09-08 10:34:21 +0800

    add class UdpReceiverAndSender add class PointerStatus add direction

--- app/src/main/java/com/ecdav/map/Net/UdpReceiverAndSender.java
+++ app/src/main/java/com/ecdav/map/Net/UdpReceiverAndSender.java
@@ -1,8 +1,14 @@
 package com.ecdav.map.Net;
 
+import java.net.DatagramSocket;
+
 /**
  * Created by iamwa on 2017/9/7.
  */
 
 public class UdpReceiverAndSender {
+    private DatagramSocket datagramSocket=null;
+    public UdpReceiverAndSender(){
+        ;
+    }
 }
